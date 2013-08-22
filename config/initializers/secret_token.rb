d9479803df543dfa7dfa4f81f6f28f35cb82c647
Étienne Vallette d'Osia <evo@zenexity.com>
2013-08-22 12:07:57 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StarterRubyRails::Application.config.secret_key_base = 'ba12193d3ae9ac8356a35244e2791143eec02ebee47af93e6247ebf15b6098525ef5d32a8671f7bb0ce2a75bec9d9a46395d89b10373c17a3a425c789d1f8592'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StarterRubyRails::Application.config.session_store :cookie_store, key: '_starter-ruby-rails_session'
