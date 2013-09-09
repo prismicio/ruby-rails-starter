6cc02f01f591da5410510c24fe4449b9279aebb9
Samy Dindane <samy@dindane.com>
2013-09-09 18:22:08 +0200

    Retrieve the ref from the query string if specified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_ref
 
   def index
-    @ref = api.ref_id_by_label('Master')
-    @documents = api.form("everything").submit(@ref.ref)
+    @documents = api.form("everything").submit(@ref)
   end
 
   def api
-    @api ||= Prismic.api("http://lesbonneschoses.prismic.io/api")
+    @api ||= Prismic.api("https://lesbonneschoses.prismic.io/api")
   end
 
+  private
+
+  def set_ref
+    @ref = params[:ref] || api.ref_id_by_label('Master').ref
+  end
 end
