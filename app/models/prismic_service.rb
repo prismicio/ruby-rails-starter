a1ccfc3438946d2327c53436591074ec021da80c
Samy Dindane <samy@dindane.com>
2013-09-20 11:45:06 +0200

    Add links to the homepage

---
+++ app/models/prismic_service.rb
@@ -0,0 +1,17 @@
+include Rails.application.routes.url_helpers
+
+module PrismicService
+  class << self
+    def get_document(id, api, ref)
+      documents = api.form("everything")
+      .query("[[:d = at(document.id, \"#{id}\")]]")
+      .submit(ref)
+
+      documents.empty? ? nil : documents.first
+    end
+
+    def init_api
+      Prismic.api("https://lesbonneschoses.prismic.io/api")
+    end
+  end
+end
