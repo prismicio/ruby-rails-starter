32b27dc9c737a9426345ac38368e29953b497079
Étienne Vallette d'Osia <evo@zenexity.com>
2013-09-27 14:39:35 +0200

    Fix submobule

--- config/initializers/prismic.rb
+++ config/initializers/prismic.rb
@@ -1,3 +1,3 @@
-PRISMIC_PATH = File.dirname(__FILE__) + "/../../lib/ruby-kit/lib"
+PRISMIC_PATH = File.dirname(__FILE__) + "/../../lib/prismic-ruby-kit/lib"
 $LOAD_PATH << PRISMIC_PATH
 require 'prismic'
