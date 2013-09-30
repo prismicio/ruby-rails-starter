9b86efcacfa3a7ddd9c60b4f544b66ad7f50fe3e
Étienne Vallette d'Osia <evo@zenexity.com>
2013-09-30 11:30:42 +0200

    Helper display_doc now provides a link_resolver
    This changes allow document links to be rendered as local URLs.

--- app/helpers/prismic_helper.rb
+++ app/helpers/prismic_helper.rb
@@ -1,5 +1,17 @@
 module PrismicHelper
 
+  class LinkResolver
+    include PrismicHelper
+    attr_reader :ref
+    def initialize(ref, &blk)
+      @ref = ref
+      @blk = blk
+    end
+    def link_to(doc_link)
+      @blk.call(id: doc_link.id, slug: doc_link.slug, ref: ref)
+    end
+  end
+
   def url_to_doc(doc, ref)
     document_path(id: doc.id, slug: doc.slug, ref: ref)
   end
@@ -7,8 +19,12 @@ module PrismicHelper
     link_to(url_to_doc(doc, ref), html_options, &blk)
   end
 
-  def display_doc(doc)
-    doc.as_html(nil).html_safe
+  def display_doc(doc, ref)
+    doc.as_html(link_resolver(ref)).html_safe
+  end
+
+  def link_resolver(ref)
+    LinkResolver.new(ref){|args| document_path(args) }
   end
 
 end
