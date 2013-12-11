a90cfc11fd6529428663d6ffca26847242592ad0
Rudy Rigot <rudy.rigot@gmail.com>
2013-12-10 16:15:27 -0800

    Adding maybe_ref, usage in link_resolver, and some explanations

--- app/helpers/prismic_helper.rb
+++ app/helpers/prismic_helper.rb
@@ -11,8 +11,14 @@ module PrismicHelper
     doc.as_html(link_resolver(ref)).html_safe
   end
 
-  def link_resolver(ref)
-    Prismic::LinkResolver.new(ref){|doc| document_path(id: doc.id, slug: doc.slug) }
+  def link_resolver(maybe_ref)
+    Prismic::LinkResolver.new(maybe_ref){|doc|
+      document_path(id: doc.id, slug: doc.slug, ref: maybe_ref)
+      # maybe_ref is not expected by document path, so it appends a ?ref=currentrefid to the URL;
+      # since maybe_ref is nil when on master ref, it appends nothing.
+      # You should do the same for every path method used in the link_resolver and elsewhere in your app,
+      # so links propagate the ref id.
+    }
   end
 
   def privileged_access?
