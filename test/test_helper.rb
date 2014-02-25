5224b130316ffb3b4ad8d10b49043fa3ab867eae
Rudy Rigot <rudy.rigot@gmail.com>
2014-02-25 10:43:10 -0800

    Dropping Active Record

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
+  # ActiveRecord::Migration.check_pending!
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   #
