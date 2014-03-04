07b99ec93a9faeeac5129edfb75932456d85757f
Étienne Vallette d'Osia <evo@zengularity.com>
2014-03-04 12:03:01 +0100

    oauth: Use Rails's helper rescue_from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  # Rescue OAuth errors for some actions
+  rescue_from Prismic::API::PrismicWSAuthError, with: :redirect_to_signin,
+                                                only: [:index, :document, :search]
+
   # Homepage action: querying the "everything" form (all the documents, paginated by 20)
   def index
     @documents = api.form("everything").submit(ref)
-  rescue Prismic::API::PrismicWSAuthError
-    redirect_to signin_path
   end
 
   # Single-document page action: mostly, setting the @document instance variable, and checking the URL
@@ -22,8 +24,6 @@ class ApplicationController < ActionController::Base
       render inline: "Document not found", status: :not_found if !@slug_checker[:redirect]
       redirect_to document_path(id, @document.slug), status: :moved_permanently if @slug_checker[:redirect]
     end
-  rescue Prismic::API::PrismicWSAuthError
-    redirect_to signin_path
   end
 
   # Search result: querying all documents containing the q parameter
@@ -31,15 +31,15 @@ class ApplicationController < ActionController::Base
     @documents = api.form("everything")
                     .query(%([[:d = fulltext(document, "#{params[:q]}")]]))
                     .submit(ref)
-  rescue Prismic::API::PrismicWSAuthError
-    redirect_to signin_path
   end
-  
+
 
   private
 
 
-  ## before_action methods
+  def redirect_to_signin
+    redirect_to signin_path
+  end
 
   # Setting @ref as the actual ref id being queried, even if it's the master ref.
   # To be used to call the API, for instance: api.form('everything').submit(ref)
